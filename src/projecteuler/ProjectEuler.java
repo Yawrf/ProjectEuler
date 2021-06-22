94a106e6702980f6bf4c9abe57a8236a6bdc13a7
Yawrf <rewilloughby@ymail.com>
2021-06-22 16:54:40 -0400

    Problem 2
    Finished Problem 2

--- src/projecteuler/ProjectEuler.java
+++ src/projecteuler/ProjectEuler.java
@@ -19,7 +19,7 @@ public class ProjectEuler {
         
         System.out.println("This project contains my progress in solving the problems at https://projecteuler.net \n");
         
-        Problem p = new Problem1();
+        Problem p = new Problem2();
         
         p.solve();
     }
