d2187bc24499559c88319da65523823f664f2e67
Yawrf <rewilloughby@ymail.com>
2021-06-22 18:01:00 -0400

    Problem 8
    Finished Problem 8

--- src/projecteuler/ProjectEuler.java
+++ src/projecteuler/ProjectEuler.java
@@ -19,7 +19,7 @@ public class ProjectEuler {
         
         System.out.println("This project contains my progress in solving the problems at https://projecteuler.net \n");
         
-        Problem p = new Problem7();
+        Problem p = new Problem8();
         
         p.solve();
     }
